be1b3eb391445d17f385ba328f90237acefbd617
Rory Eiffe <eifferblue1@gmail.com>
2020-11-28 16:49:14 -0500

    Since we are still figuring out the rethink issue, I wrote some code to display the different rooms, using placeholder variables (state.rooms).

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import r from 'rethinkdb';
+// import r from 'rethinkdb';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Lobby from './Lobby';
 
 const dbname = (__DEV__) ? 'test' : 'prod';
-const conn = r.connect({ host: 'rushsteve1.us', port: 32769, db: dbname });
+// const conn = r.connect({ host: 'rushsteve1.us', port: 32769, db: dbname });
 
 export const ConnectionContext = React.createContext();
 const Stack = createStackNavigator();
 
 export default function App() {
     return (
-      <ConnectionContext.Provider value={conn}>
+      <ConnectionContext.Provider >
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Lobby">
             <Stack.Screen name="Lobby" component={Lobby}/>
--- Lobby.js
+++ Lobby.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
-import r from 'rethinkdb';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  Image,
+} from "react-native";
+// import r from 'rethinkdb';
 
 export default class Lobby extends React.Component {
   state = {
-    rooms: [],
+    rooms: [
+      { id: 1, name: "Rory's room", occupants: 4, capacity: 10 },
+      { id: 2, name: "Steven's room", occupants: 5, capacity: 10 },
+    ],
     friends: [],
-  }
+  };
 
   constructor() {
     super();
@@ -14,17 +23,40 @@ export default class Lobby extends React.Component {
   }
 
   roomPress = (roomId) => {
-    // Do a thing
-  }
+    console.log(roomId + " was pressed.");
+    alert("you pressed the button");
+  };
 
   render() {
     return (
       <View style={styles.container}>
-      {this.state.rooms.map((room) => {
-        <TouchableHighlight
-        onPress={roomPress(room.id)}>
-        <TouchableHighlight/>
-      })}
+        <Text style={{ marginBottom: 10 }}>Chats near you</Text>
+        <View style = {styles.rooms}>
+          {this.state.rooms.map((room) => (
+            <TouchableHighlight onPress={this.roomPress.bind(room.id)}>
+              <View style={styles.room}>
+                <View style={styles.roomInfo}>
+                  <Text style={styles.roomName}>{room.name}</Text>
+                  <View
+                    style={{ flexDirection: "row", justifyContent: "center" }}
+                  >
+                    <Image
+                      style={styles.image}
+                      source={{
+                        uri:
+                          "https://www.tuktukdesign.com/wp-content/uploads/2020/01/people-icon-vector.jpg",
+                      }}
+                    />
+                    <Text style={styles.occupancy}>
+                      {room.occupants}/{room.capacity}
+                    </Text>
+                  </View>
+                  <Text style={styles.join}>Join</Text>
+                </View>
+              </View>
+            </TouchableHighlight>
+          ))}
+        </View>
       </View>
     );
   }
@@ -33,8 +65,42 @@ export default class Lobby extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 5,
+  },
+  rooms: {
+  },
+  room: {
+    height: 200,
+    marginBottom: 10,
+    backgroundColor: "#C4C4C4",
+    elevation: 5,
+  },
+  roomInfo: {
+    bottom: 0,
+    marginTop: "auto",
+    backgroundColor: "#fff",
+    paddingBottom: 10,
+    paddingTop: 10,
+  },
+  roomName: {
+    color: "#7C4DFF",
+    textAlign: "center",
+  },
+  occupancy: {
+    textAlign: "center",
+  },
+  image: {
+    width: 20,
+    height: 20,
+    resizeMode: "stretch",
+  },
+  join: {
+    color: "#7C4DFF",
+    textAlign: "right",
+    paddingRight: 20,
+    fontSize: 15,
   },
 });
